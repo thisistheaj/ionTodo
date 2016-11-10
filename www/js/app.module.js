74363a60cdb31b13831ece819f4a6f92934cb379
AJ Beckner <ajbeckner@gmail.com>
2016-11-09 20:38:19 -0500

    separated playlists controller

--- www/js/app.module.js
+++ www/js/app.module.js
@@ -1,4 +1,5 @@
-angular.module('starter', [
+angular.module('ionTodos', [
   'ionic',
-  'starter.controllers'
+  'ionTodos.controllers',
+  'ionTodos.playlists'
 ]);
--- www/js/app.routes.js
+++ www/js/app.routes.js
@@ -1,4 +1,4 @@
-angular.module('starter')
+angular.module('ionTodos')
 
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
